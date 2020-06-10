5fac801a48a5fa7cec4efdc3e61372999c9c26b6
Paweł Pietrzak <s18793@pjwstk.edu.pl>
2020-06-10 18:23:34 +0200

    zaczynam 1 koncówke

---
+++ cw13/DTOs/resp/OrderRespon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw13.DTOs.resp
+{
+    public class OrderRespon
+    {
+        
+        public int IdZamowienia { get; set; }
+        public int IdWyrobu { get; set; }
+        public DateTime DataPrzyjecia { get; set; }
+        public string Uwagi { get; set; } 
+        public string Nazwa { get; set; }
+        public double CenaZaSzt { get; set; }
+        public string Typ { get; set; }
+    }
+}
---
+++ cw13/Services/ICukierniaDb.cs
@@ -0,0 +1,16 @@
+﻿using cw13.DTOs.resp;
+using cw13.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw13.Services
+{
+    public interface ICukierniaDb
+    {
+
+        public IEnumerable<OrderRespon> getOrders(String nazwisko);
+
+    }
+}
