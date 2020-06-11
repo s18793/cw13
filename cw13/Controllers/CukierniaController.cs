22f46d04a58f0c802109abc3077abb569126a84c
Paweł Pietrzak <s18793@pjwstk.edu.pl>
2020-06-11 17:20:09 +0200

    Zwracam Zamowienia cw13

--- cw13/Controllers/CukierniaController.cs
+++ cw13/Controllers/CukierniaController.cs
@@ -2,14 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using cw13.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace cw13.Controllers
 {
     public class CukierniaController : ControllerBase
 
+
     {
-       
-       
+
+        private readonly ICukierniaDb ic;
+
+
+        public CukierniaController(ICukierniaDb dv)
+        {
+            ic = dv;
+        }
+        [HttpGet]
+        public IActionResult getOrders(String nazwisko)
+        {
+            var order = ic.getOrders(nazwisko);
+            return Ok(order);
+        }
     }
 }
--- cw13/Services/EfCukierniaDb.cs
+++ cw13/Services/EfCukierniaDb.cs
@@ -1,11 +1,57 @@
-﻿using System;
+﻿using cw13.DTOs.resp;
+using cw13.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace cw13.Services
 {
-    public class EfCukierniaDb
+    public class EfCukierniaDb : ICukierniaDb
     {
+        private readonly CukierniaContext _con;
+
+        public EfCukierniaDb(CukierniaContext context)
+        {
+            _con = context;
+        }
+        public IEnumerable<OrderRespon> getOrders(string nazwisko)
+        {
+            var klient = _con.Klients.FirstOrDefault(e => e.Nazwisko == nazwisko);
+
+            var client = from Zamowienie_WyrobCukierniczy in _con.Zamowienia_WyrobCukiernicze
+                       join Zamowienie in _con.Zamowienia on Zamowienie_WyrobCukierniczy.IdZamowienia equals Zamowienie.IdZamowienia
+                       join WyrobCukierniczy in _con.WyrobyCukiernicze on Zamowienie_WyrobCukierniczy.IdWyrobu equals WyrobCukierniczy.IdWyrobu
+                       where Zamowienie.IdKlient== klient.IdKlient
+                         select new OrderRespon()
+                         {
+                             IdWyrobu = Zamowienie.IdZamowienia,
+                             CenaZaSzt = WyrobCukierniczy.CenaZaSzt,
+                             DataPrzyjecia = Zamowienie.DataPrzyjecia,
+                             Uwagi = Zamowienie.Uwagi,
+                             Typ = WyrobCukierniczy.Typ
+                         };
+            var lista = client.ToList();
+            return lista;
+
+
+            if (nazwisko == null) {
+                var find = from Zamowienie_WyrobCukierniczy in _con.Zamowienia_WyrobCukiernicze
+                           join Zamowienie in _con.Zamowienia on Zamowienie_WyrobCukierniczy.IdZamowienia equals Zamowienie.IdZamowienia
+                           join WyrobCukierniczy in _con.WyrobyCukiernicze on Zamowienie_WyrobCukierniczy.IdWyrobu equals WyrobCukierniczy.IdWyrobu
+                           select new OrderRespon()
+                           {
+                               IdWyrobu = Zamowienie.IdZamowienia,
+                               CenaZaSzt = WyrobCukierniczy.CenaZaSzt,
+                               DataPrzyjecia = Zamowienie.DataPrzyjecia,
+                               Uwagi = Zamowienie.Uwagi,
+                               Typ = WyrobCukierniczy.Typ
+                           };
+                var orderList = find.ToList();
+                return orderList;
+
+                }
+                        
+        }
     }
 }
